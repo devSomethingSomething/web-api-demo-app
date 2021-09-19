790f63c13f569ea2e7c6f09bc468ca6d2fb5e1cd
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-09-19 14:10:40 +0200

    Add new controller with action method

---
+++ WebApiDemoApp/Controllers/RecipesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetDishes()
+        {
+            string[] dishes = { "Dish 1", "Dish 2", "Dish 3" };
+
+            return dishes;
+        }
+    }
+}
